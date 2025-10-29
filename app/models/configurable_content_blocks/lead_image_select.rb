7fa26559087da9d38242d0fc732bbac5da2d9e2e
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-10-29 14:24:11 +0000

    Send placeholder url in the payload
    This changes behaviour from previous implementation:
    - We not only render the placeholder image wh-side, but we also send the hardcoded url in the payload.
    - We show the placeholder if there is no custom lead, nor default lead image.
    - We now also show the placeholder if the assets - for custom lead or default lead image - have not finished uploading/have assets missing (rather than sending nothing). This matches what the FE app would do now, i.e. render the placeholder when we send nothing if the assets are missing.
    
    Reasoning:
    - This will allow us to remove surprising code from frontend, where this logic is duplicated.
    - We use the placeholder to render it wh-side anyway. If we maintain it in two different places, we risk FE changing it out of sync with WH, and showing the publisher an image that might not appear on the rendered page.
    - WH is the "source of truth" in regard to what imagery it displays.
    
    History of the change:
    - The placeholder behaviour has recently been removed (b7be3164) from wh since it's duplicated on FE, but we're now opting to bring this back into WH with a view to eventually remove the duplication from FE instead.
    
    Note on future changes:
    - The lead image behaviour is being explored with users via Basecamp. No changes will be made on FE as of now. We can make a change once we've made all the content types' lead image behaviour consistent - i.e., speeches and case studies.
    - Showing the placeholder when assets are missing can be dropped if we don't have a need to show "some lead image". We'd need to be more explicit with the user about an assets-missing failure, unsure what the "correct" behaviour should be here. The asset versioning is also potentially in the works, so not making any strong decisions here.

--- app/models/configurable_content_blocks/lead_image_select.rb
+++ app/models/configurable_content_blocks/lead_image_select.rb
@@ -9,20 +9,22 @@ module ConfigurableContentBlocks
 
     def publishing_api_payload(content)
       selected_image = images.find { |image| image.image_data.id == content.to_i }
-      if selected_image&.image_data&.all_asset_variants_uploaded?
+      if selected_image
+        if selected_image.image_data&.all_asset_variants_uploaded?
+          return {
+            high_resolution_url: selected_image.image_data.url(:s960),
+            url: selected_image.image_data.url(:s300),
+            caption: lead_image_caption(selected_image),
+          }.compact
+        end
+      elsif default_lead_image&.all_asset_variants_uploaded?
         return {
-          high_resolution_url: selected_image.image_data.url(:s960),
-          url: selected_image.image_data.url(:s300),
-          caption: lead_image_caption(selected_image),
-        }.compact
-      end
-
-      if default_lead_image&.all_asset_variants_uploaded?
-        {
           high_resolution_url: default_lead_image.url(:s960),
           url: default_lead_image.url(:s300),
         } # default images do not have captions
       end
+
+      placeholder_payload
     end
 
     def to_partial_path
@@ -40,7 +42,14 @@ module ConfigurableContentBlocks
     end
 
     def placeholder_image_url
-      ActionController::Base.helpers.image_url("placeholder.jpg", host: Whitehall.public_root)
+      "https://assets.publishing.service.gov.uk/media/5e59279b86650c53b2cefbfe/placeholder.jpg"
+    end
+
+    def placeholder_payload
+      {
+        high_resolution_url: placeholder_image_url,
+        url: placeholder_image_url,
+      }
     end
 
     def lead_image_caption(image)
